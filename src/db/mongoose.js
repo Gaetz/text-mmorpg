9faad7e34890f931b548a87dfa89940a11f47540
Gaetz <gaetz@rufflerim.com>
2020-05-17 21:41:49 +0200

    No change

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/text-mmor
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-});
+})
 
 /* 1. Use model */ 
 
